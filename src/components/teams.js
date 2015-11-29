a1969b65d856d46015247ccdd0a0b91d00c3ba94
Simon Wade <simon.wade@gmail.com>
2015-11-29 19:39:06 +1100

    Display teams using a ListView and added react-native-icons for chevrons

--- src/components/teams.js
+++ src/components/teams.js
@@ -2,14 +2,20 @@ import React from 'react-native';
 import Services from '../services';
 import Button from 'react-native-button';
 import ReactFireMixin from 'reactfire';
+import inflection from 'inflection';
 import {
-  handleError
+  handleError,
 } from '../utils';
+import {
+  Icon,
+} from 'react-native-icons';
 let {
   StyleSheet,
   PropTypes,
   View,
-  Text
+  Text,
+  ListView,
+  TouchableHighlight,
 } = React;
 
 export default React.createClass({
@@ -18,34 +24,83 @@ export default React.createClass({
   },
   mixins: [ReactFireMixin],
   getInitialState() {
-    return {teams: []};
+    return {
+      teams: []
+    };
   },
   componentWillMount() {
     this.store = Services.get('store');
     this.teams = Services.get('teams');
     this.bindAsArray(this.teams.forUser(this.props.userRef), 'teams');
+    //this.state.teams = [
+    //  {'.key': 'jtribe'},
+    //  {'.key': 'foo'},
+    //  {'.key': 'barbaz'},
+    //];
+  },
+  dataSource() {
+    let ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+    let refs = this.state.teams.map(team => this.store.ref('team', team['.key']));
+    return ds.cloneWithRows(refs);
   },
   render() {
-    var teams = this.state.teams.map(team => <Text key={team['.key']}>{team['.key']}</Text>);
     return (
       <View style={styles.container}>
-        <View style={styles.teams}>
-          {teams}
-        </View>
+        <ListView style={styles.teams}
+          dataSource={this.dataSource()}
+          renderRow={this.renderRow}
+        />
         <View style={styles.footer}>
-          <Button onPress={this.joinTeam}>Join Team</Button>
+          <Button onPress={this.props.addTeam}>Add Team</Button>
         </View>
       </View>
     )
   },
-  joinTeam() {
-    var attrs = {name: 'jtribe'}; // ask the user for this
-    this.teams.findOrCreateTeam(attrs)
-      .then(teamRef => this.teams.join(teamRef))
-      .catch(handleError);
+  renderRow(teamRef, sectionID, rowID) {
+    return (
+      <ListViewItem key={teamRef.key()} teamRef={teamRef}></ListViewItem>
+    )
   },
 });
 
+let ListViewItem = React.createClass({
+  mixins: [ReactFireMixin],
+  getInitialState() {
+    return {
+      team: {
+        members: []
+      }
+    };
+  },
+  componentWillMount() {
+    this.bindAsObject(this.props.teamRef, 'team');
+    //this.state.team = {
+    //  name: this.props.teamRef.key(),
+    //  members: Array(this.props.teamRef.key().length)
+    //};
+  },
+  render() {
+    let team = this.state.team;
+    let numMembers = Object.keys(team.members).length;
+    return (
+      <TouchableHighlight onPress={() => this.props.onPress(this.teamRef)} underlayColor='#efefef'>
+        <View style={styles.listItem}>
+          <View>
+            <Text style={styles.name}>{team.name}</Text>
+            <Text style={styles.members}>
+              {`${numMembers} ${inflection.inflect('member', numMembers)}`}
+            </Text>
+          </View>
+          <Icon name='ion|chevron-right' color='#ccc' size={iconSize} style={styles.disclosure} />
+        </View>
+      </TouchableHighlight>
+    )
+  }
+});
+
+let iconSize = 20;
 let styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -55,7 +110,30 @@ let styles = StyleSheet.create({
   teams: {
     flex: 1,
   },
+  listItem: {
+    borderBottomWidth: 1 / React.PixelRatio.get(),
+    borderColor: '#ccc',
+    padding: 10,
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  name: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  members: {
+    fontSize: 15,
+    fontWeight: '100',
+  },
+  disclosure: {
+    width: iconSize,
+    height: iconSize,
+    marginTop: 12,
+    alignItems: 'center',
+  },
   footer: {
     alignItems: 'center',
+    padding: 10,
   },
 });
