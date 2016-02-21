b9dcdd6256ee2c63c84da537f02041d011d439da
Madeleine Mihailides <madeleine@jtribe.com.au>
2016-02-22 10:41:56 +1100

    Updated the config.

---
+++ ios/.config.sh
@@ -0,0 +1,9 @@
+export PROJECT="Pomodoro"
+# export WORKSPACE="$PROJECT.xcworkspace" # Comment this out for no workspace
+export SCHEME="$PROJECT"
+export TEST_SCHEME="${PROJECT}Tests"
+export UI_TEST_SCHEME="${PROJECT}UITests" # Comment this out for no UI tests
+export TEST_DESTINATION="platform=iOS Simulator,name=iPhone 6,OS=9.2"
+
+export BUNDLE_IDENTIFIER="au.com.jtribe.pomoranian"
+export ITC_USER="armin@jtribe.com.au" # iTunes Connect User
